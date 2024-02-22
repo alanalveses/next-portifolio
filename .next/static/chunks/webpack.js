af95d84e2d1b1fc3d8ec3ac53d32c60c89e4aeb4
Alan Alves <alan.alveses@gmail.com>
2024-02-21 23:17:19 -0300

    add components "NavBar" and "Logo"

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff213412c7ea2b51"; }
+/******/ 		__webpack_require__.h = function() { return "00916e2282def081"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import NavBar from "@/components/NavBar";
 import "@/styles/globals.css";
 
 import { Montserrat } from "next/font/google";
@@ -16,8 +17,9 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${montserrat.variable} font-sans bg-light w-full min-h-screen`}>
+        <NavBar/>
         <Component {...pageProps} />
       </main>
     </>
-  );
+  )
 }
